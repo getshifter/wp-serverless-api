943a135bb9807517d8bebc22560dc16c674f25d4
Daniel Olson <emaildano@gmail.com>
2018-07-12 23:19:26 -0400

    updated array build

--- wp-serverless-api.php
+++ wp-serverless-api.php
@@ -19,7 +19,7 @@ function compile_db(
     $db_array = array();
 
     foreach ($routes as $route) {
-        $url =  'https://demo.wp-api.org/wp-json/wp/v2/' . $route . '?_embed&per_page=5';
+        $url =  esc_url( home_url( '/' ) ) . 'wp-json/wp/v2/' . $route . '?_embed&per_page=5';
         $jsonData = json_decode( file_get_contents($url) );
         $db_array[] = $jsonData;
     }
