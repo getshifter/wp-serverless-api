a64f45914fa8a91c78a13c176c089c864257c5ee
Daniel Olson <emaildano@gmail.com>
2018-07-13 00:40:44 -0400

    updated array format for routes

--- wp-serverless-api.php
+++ wp-serverless-api.php
@@ -19,9 +19,11 @@ function compile_db(
     $db_array = array();
 
     foreach ($routes as $route) {
-        $url =  esc_url( home_url( '/' ) ) . 'wp-json/wp/v2/' . $route . '?_embed&per_page=5';
+        $url =  'https://demo.wp-api.org/wp-json/wp/v2/' . $route;
+        // $url =  esc_url( home_url( '/' ) ) . 'wp-json/wp/v2/' . $route . '?_embed&per_page=5';
         $jsonData = json_decode( file_get_contents($url) );
-        $db_array[] = $jsonData;
+
+        $db_array[$route] = (array) $jsonData;
     }
 
     $db = json_encode($db_array);
