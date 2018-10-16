8d8dec390e5f2043c8a7dd4e2c52abf78864003f
Daniel Olson <emaildano@gmail.com>
2018-10-16 15:41:00 -0400

    added permalink notice, updated URL on Shifter Local

--- wp-serverless-api.php
+++ wp-serverless-api.php
@@ -7,6 +7,17 @@ Author URI: https://github.com/emaildano/wp-serverless-api
 Description: WordPress REST API to JSON File
 */
 
+function enable_permalinks_notice() {
+    ?>
+    <div class="notice notice-warning">
+        <p><?php _e( 'WP Serverless Redirects requires Permalinks. <a href="/wp-admin/options-permalink.php">Enable Permalinks</a>'); ?></p>
+    </div>
+    <?php
+}
+
+if ( !get_option('permalink_structure') ) {
+    add_action( 'admin_notices', 'enable_permalinks_notice' );
+}
 
 function compile_db(
     $routes = array(
@@ -19,7 +30,12 @@ function compile_db(
     $db_array = array();
 
     foreach ($routes as $route) {
-        $url =  'https://demo.wp-api.org/wp-json/wp/v2/' . $route;
+        if  (getenv("SHIFTER_ACCESS_TOKEN") === false) {
+            $url =  'https://demo.wp-api.org/wp-json/wp/v2/' . $route;
+        } else {
+            $url =  '/wp-json/wp/v2/' . $route;
+        }
+
         $jsonData = json_decode( file_get_contents($url) );
 
         $db_array[$route] = (array) $jsonData;
@@ -56,12 +72,3 @@ function build_db()
 }
 
 add_action( 'save_post', 'build_db' );
-
-/**
- * Create JSON on Activation
- */
-register_activation_hook( __FILE__, 'pluginprefix_install' );
-function pluginprefix_install()
-{
-    build_db();
-}
