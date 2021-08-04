9be5cf2eeafe4ddd5a9cf1fbab937754cf8097fe
Daniel Olson <emaildano@gmail.com>
2021-08-04 12:06:24 -0400

    updated rest support

--- README.md
+++ README.md
@@ -3,11 +3,23 @@
 Explore WordPress data via the WP REST API as a JSON file for static WordPress Hosting on [Shifter](https://getshifter.io).
 
 1. Install as a WordPress Plugin
-2. Activate and save or create a new post or page
-3. Create a new static Artifact on Shifter
-4. Visit your new WP Serverless API endpoint at `example.com/wp-content/wp-sls-api/db.json`
+2. Visit any single valid WP REST API endpoint
+
+# Considerations
+
+This plugin works best with headless applications. Its design mirrors and writes REST requests to file within the uploads folder. Currently, it only supports single post requests, such as /posts/1.
+
+When using this plugin on Shifter, the static JSON file is synced to the CDN, which allows access to the data, whether WordPress is running or not.
+
+# Roadmap
+
+- Add support for query params, including per_page to support post arrays.
 
 ## CHANGELOG
+
+### 0.3.0
+
+- [BREAKING CHANGE] Change save path from `/wp-content/wp-sls-api/db.json` to `/wp-content/wp-json/wp/v2/{post_type}/{id}`
 
 ### 0.2.0
 
--- wp-serverless-api.php
+++ wp-serverless-api.php
@@ -9,49 +9,21 @@ Author: Shifter
 Author URI: https://getshifter.io
 */
 
-function enable_permalinks_notice()
-{
-?>
-    <div class="notice notice-warning">
-        <p><?php _e('WP Serverless Redirects requires Permalinks. <a href="/wp-admin/options-permalink.php">Enable Permalinks</a>'); ?></p>
-    </div>
-<?php
-}
-
-if (!get_option('permalink_structure')) {
-    add_action('admin_notices', 'enable_permalinks_notice');
-}
-
-function compile_db($route)
+function wp_sls_api(array $result, \WP_REST_Server $server, \WP_Rest_Request $request): array
 {
 
-    $db_array = array();
+    $route = $request->get_route();
+    $request = new WP_REST_Request('GET', $request->get_route());
+    $response = rest_do_request($request);
+    $server = rest_get_server();
+    $data = (array) $server->response_to_data($response, false);
 
-    $url =  esc_url(home_url('/')) . 'wp-json/wp/v2/' . $route;
+    // Check for routing errors.
+    if ($data['data']['status'] === 404) {
+        return $result;
+    }
 
-    $arrContextOptions = array(
-        "ssl" => array(
-            "verify_peer" => false,
-            "verify_peer_name" => false,
-        ),
-    );
-
-    $response = file_get_contents($url, false, stream_context_create($arrContextOptions));
-
-    $jsonData = json_decode($response);
-
-    $db_array = $jsonData;
-
-    $db = json_encode($db_array);
-
-    return $db;
-}
-
-function save_db(
-    $db,
-    $file_name = 'posts'
-) {
-    $save_path = WP_CONTENT_DIR . '/uploads/wp-json/wp/v2/' . $file_name . '.json';
+    $save_path = WP_CONTENT_DIR . '/uploads/wp-json' . $route . '.json';
     $dirname = dirname($save_path);
 
     if (!is_dir($dirname)) {
@@ -59,17 +31,12 @@ function save_db(
     }
 
     $f = fopen($save_path, "w+");
-    fwrite($f, $db);
+    fwrite($f, json_encode($data));
     fclose($f);
+
+    // return (array) $request->get_params();
+    // return (array) $request->get_route();
+    return $result;
 }
 
-function build_db($post_id)
-{
-    $db = compile_db('posts/' . $post_id);
-    save_db($db, $post_id);
-}
-
-/**
- * Build on Post Save
- */
-add_action('save_post', 'build_db');
+add_filter('rest_pre_echo_response', 'wp_sls_api', 10, 3);
