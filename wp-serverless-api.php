9c130d82c10e51729b28e83707528d88f8e5b240
Daniel Olson <emaildano@gmail.com>
2021-08-13 18:44:21 -0400

    Removing user trigger

--- wp-serverless-api.php
+++ wp-serverless-api.php
@@ -37,57 +37,55 @@ function api_request(array $result, \WP_REST_Server $server, \WP_Rest_Request $r
 
         return $result;
     }
-
-    return $result;
 }
 
 add_filter('rest_pre_echo_response', 'api_request', 10, 3);
 
 
-function user_request($post_id)
-{
+// function user_request($post_id)
+// {
 
-    $post = get_post($post_id);
+//     $post = get_post($post_id);
 
-    if (wp_is_post_revision($post_id)) {
-        return;
-    }
+//     if (wp_is_post_revision($post_id)) {
+//         return;
+//     }
 
-    if (wp_is_post_autosave($post_id)) {
-        return;
-    }
+//     if (wp_is_post_autosave($post_id)) {
+//         return;
+//     }
 
-    if (in_array($post->post_type, ['post', 'page'])) {
+//     if (in_array($post->post_type, ['post', 'page'])) {
 
-        $result = [];
-        $post_type = get_post_type($post_id);
-        $post_type_obj = get_post_type_object($post_type);
-        $post_type_name = (string) strtolower($post_type_obj->labels->name);
+//         $result = [];
+//         $post_type = get_post_type($post_id);
+//         $post_type_obj = get_post_type_object($post_type);
+//         $post_type_name = (string) strtolower($post_type_obj->labels->name);
 
-        $rest_path = '/wp/v2/' .  $post_type_name . '/' . $post_id;
+//         $rest_path = '/wp/v2/' .  $post_type_name . '/' . $post_id;
 
-        $route = $rest_path;
-        $request = new WP_REST_Request('GET', $route);
+//         $route = $rest_path;
+//         $request = new WP_REST_Request('GET', $route);
 
-        $response = rest_do_request($request);
-        $server = rest_get_server();
-        $data = (array) $server->response_to_data($response, false);
-        $save_path = WP_CONTENT_DIR . '/uploads/wp-json/' . $route . '.json';
-        $f = fopen($save_path, "w+");
-        $dirname = dirname($save_path);
+//         $response = rest_do_request($request);
+//         $server = rest_get_server();
+//         $data = (array) $server->response_to_data($response, false);
+//         $save_path = WP_CONTENT_DIR . '/uploads/wp-json/' . $route . '.json';
+//         $f = fopen($save_path, "w+");
+//         $dirname = dirname($save_path);
 
-        // Check and make file directory.
-        if (!is_dir($dirname)) {
-            mkdir($dirname, 0755, true);
-        }
+//         // Check and make file directory.
+//         if (!is_dir($dirname)) {
+//             mkdir($dirname, 0755, true);
+//         }
 
-        fwrite($f, json_encode($data));
-        fclose($f);
+//         fwrite($f, json_encode($data));
+//         fclose($f);
 
-        // error_log(print_r($request, true));
+//         // error_log(print_r($request, true));
 
-        return $result;
-    }
-}
+//         return $result;
+//     }
+// }
 
-add_action('save_post', 'user_request');
+// add_action('save_post', 'user_request');
